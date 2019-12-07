69b9c2107d696c86cdfd7750ec48a08f463ea26b
Adrian-Garcia <adriangarlop@hotmail.com>
2019-12-07 14:58:52 -0600

    Added dissemonation-model.js structure. It is ready for implementation

---
+++ models/dissemination-model.js
@@ -0,0 +1,51 @@
+let mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let disseminationSchema = mongoose.Schema({
+	id					: 		{type: String, require: true},
+	name				: 		{type: String, require: true},
+	message			: 		{type: String, require: false},
+	companys		: 		{type: [String], require: false},
+	ambassador	: 		{type: [String], require: false},
+	stuff				: 		{type: [String], require: false}
+});
+
+let Dissemination = mongoose.model('disseminations', disseminationSchema);
+
+let Disseminations = {
+
+	get: function() {
+		return Dissemination.find().then(disseminations => {
+			return disseminations;
+		}).catch(error => {
+			throw Error(error);
+		});
+	},
+
+	post: function(createdDissemination) {
+		return Dissemination.create(createdDissemination).then(dissemination => {
+			return dissemination;
+		}).catch(error => {
+			throw Error(error);
+		});
+	},
+
+	update: function(updatedDissemination) {
+		return Dissemination.updateOne({id:updatedDissemination.id}, updatedDissemination).then(dissemination => {
+			return dissemination;
+		}).catch(error => {
+			throw Error(error);
+		});
+	},
+
+	delete: function(idDissemination) {
+		return Dissemination.findOneAndRemove({id:idDissemination}).then(dissemination => {
+			return dissemination;
+		}).catch(error => {
+			throw Error(error);
+		})
+	}
+};
+
+module.exports = {Disseminations}
